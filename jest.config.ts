e42d511881561e429afe079999e8d71171397a27
Stanislav Povolotsky <stas.github@povolotsky.info>
2024-04-03 01:40:37 +0200

    Test test

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,18 @@
 import type { Config } from 'jest';
 
+const reporters = ["default"];
+if (process.env.CI) {
+  reporters.push("github-actions");
+}
+
 const config: Config = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
     cache: false,
     verbose: true,
-    testTimeout: 60000
+    testTimeout: 60000,
+    reporters: reporters
 };
 
 export default config;
