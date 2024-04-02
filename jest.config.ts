cdcc4769bf43b1289724abf1dce12f4513339dcb
Stanislav Povolotsky <stas.github@povolotsky.info>
2024-04-03 01:14:32 +0200

    Test test

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,9 @@ const config: Config = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+    cache: false,
+    verbose: true,
+    testTimeout: 60000
 };
 
 export default config;
